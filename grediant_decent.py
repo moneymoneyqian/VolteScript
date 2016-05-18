bf6bed7f30d8a2c82cfd8078585d587f5f1dbd10
unknown <moneymoneyqian@163.com>
2016-05-18 15:45:24 +0800

    add some discription

--- grediant_decent.py
+++ grediant_decent.py
@@ -6,14 +6,18 @@ X = pd.read_csv('E:/sample/5.1.logistic_x.txt',sep=',',header=None,names=('v1','
 y = pd.read_csv('E:/sample/5.1.logistic_y.txt',sep=',',header=None,names=('y'))
 
 
+# #use sklearn to fit model
+# from sklearn.linear_model import LogisticRegression
+# X = pd.read_csv('E:/sample/5.1.logistic_x.txt',sep=',',header=None,names=('v1','v2'))
+# y = pd.read_csv('E:/sample/5.1.logistic_y.txt',sep=',',header=None,names=('y'))
+# model = LogisticRegression(solver='newton-cg')
+# model.fit(X,y)
+
+# grediant decent to find best theta
 
 
 
-
-
-
-
-
+# grediant decent to find best theta
 
 X=np.array(X)
 plt.scatter(X[:,0], X[:,1], c=np.array(y), cmap=plt.cm.Paired)
